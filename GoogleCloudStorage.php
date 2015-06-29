74dc149d9a12aa8fdcec67ee06f943bc36907aaa
Monfee <eric.tang2005@gmail.com>
2015-06-29 13:18:46 +1000

    init import

---
+++ GoogleCloudStorage.php
@@ -0,0 +1,59 @@
+<?php 
+global $wgRunOnGae, $wgUploadPath, $wgUploadDirectory;
+
+$wgGaeHome = '';
+if ( !$wgRunOnGae )
+	$wgGaeHome = '/data/tools/GAE/google_appengine/php/sdk/';
+
+$wgCssProtocol = "gs";
+
+if ($wgRunOnGae || $wgUseGoogleStorage === true) {
+	$wgCloudStorageUploadPath = 'images';
+}
+else {
+	$wgCssProtocol = "file";
+	$wgCloudStorageBucket = '';
+	//$wgCloudStorageUploadPath = $wgUploadDirectory;
+	$wgCloudStorageUploadPath = "$IP/images";
+}
+
+$wgFsBackend = $wgCssProtocol;
+
+$wgCloudStorageBaseUrl = $wgCssProtocol . "://";
+$wgCloudStorageUrl = $wgCloudStorageBaseUrl . $wgCloudStorageBucket . '/';
+$wgCloudStorageDirectory = $wgCloudStorageUrl . $wgCloudStorageUploadPath;
+
+/*******************************************************************************************************************
+		'backend' => $wgFsBackend,
+
+		'url' => $wgCloudStorageUrl ? $wgCloudStorageUrl . $wgCloudStorageUploadPath : $wgCloudStorageUploadPath,
+		'urlbase' => $wgCloudStorageBaseUrl ? $wgCloudStorageBaseUrl : "",
+		'hashLevels' => $wgHashedUploadDirectory ? 2 : 0,
+		'thumbScriptUrl' => $wgThumbnailScriptPath,
+		'transformVia404' => !$wgGenerateThumbnailOnParse,
+		'initialCapital' => $wgCapitalLinks,
+		'deletedDir' => $wgCloudStorageDirectory.'/deleted',
+		'deletedHashLevels' => $wgHashedUploadDirectory ? 3 : 0,
+ ********************************************************************************************************************/
+$wgLocalFileRepo = array(
+		'class' => 'GoogleCloudStorageRepo',
+		'name' => 'gs',
+		'directory' => $wgCloudStorageDirectory,
+		'scriptDirUrl' => $wgScriptPath,
+		'scriptExtension' => $wgScriptExtension,
+		'url' => $wgUploadBaseUrl ? $wgUploadBaseUrl . $wgUploadPath : $wgUploadPath,
+		'thumbUrl' => $wgUploadThumbUrl ? $wgUploadThumbUrl : $wgUploadPath . '/thumb',
+		'bucket' => $wgCloudStorageBucket
+);
+
+if (!class_exists('GoogleCloudStorageService')) require_once( "GoogleCloudStorageService.php");
+
+# Cloud Storage Service instance
+$wgCss = new GoogleCloudStorageService();
+$wgCss->setProtocol($wgCssProtocol);
+$wgCss->setBucketName($wgCloudStorageBucket);
+
+require_once("CloudStorageRepo.php");
+require_once("GoogleCloudStorageFile.php");
+require_once("CloudStorageFileArchive.php");
+require_once("GoogleCloudStorageRepo.php");
