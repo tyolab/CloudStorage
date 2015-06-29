e560a0c4bd2c7ed1e6e90d79652b1d68c925c3b7
Monfee <eric.tang2005@gmail.com>
2015-06-29 13:26:16 +1000

    add a few copyright information

--- CloudStorage.php
+++ CloudStorage.php
@@ -1,4 +1,24 @@
 <?php
+/******************************************************************************
+ * This file is part of the MediaWiki/extensions/CloudStorage extension.
+ *
+ * (c) Copyright 2015 TYONLINE TECHNOLOGY PTY. LTD.
+ *
+ * This file may be distributed and/or modified under the terms of the
+ * GNU LESSER GENERAL PUBLIC LICENSE, Version 3 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.LGPL included in the
+ * packaging of this file.
+ *
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ *
+ *******************************************************************************
+ *
+ * @created  		27/Jun/2015
+ * @author			Eric Tang
+ *
+ *******************************************************************************/
 
 $wgCloundStorageDriver = "gs";
 
